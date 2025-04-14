a7fa234281dfd175ce4340eed2da33cc172fc467
John Theo <johntheo@skyscanner.net>
2025-04-14 21:07:53 +0200

    capture pageview

--- app/providers.tsx
+++ app/providers.tsx
@@ -14,7 +14,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
       posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
         api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
         person_profiles: 'identified_only',
-        capture_pageview: false,
+        capture_pageview: true,
         debug: process.env.NODE_ENV === 'development',
         persistence: 'localStorage',
         autocapture: true,
